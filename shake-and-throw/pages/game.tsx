ba80b020088b095256dea3b8020ff8dac5e4a924
abokado2525 <yuushinhana@gmail.com>
2023-06-17 18:42:11 +0900

    pullするためのcommit

--- shake-and-throw/pages/game.tsx
+++ shake-and-throw/pages/game.tsx
@@ -1,26 +1,39 @@
 import Link from "next/link";
-import ReactPlayer from "react-player";
+import React from "react";
+import { Player } from "video-react";
+
+//TODO Q. abokado branchにmainのpullできてるのに反映されないなぜ
+//TODO A. pullする前にローカルファイル変更したから。git stash save  git pull origin main git stash pop
+//TODO 動画の埋め込み
+//TODO 加速度取得
+//TODO ngrok acount touroku
+
+// window.addEventListener("devicemotion", Game);
 
 // pages/about.js
-function About() {
+function Game(event: DeviceMotionEvent) {
+  // event.accelerationがnull出ない場合、加速度データを取得
+  const { x, y, z } = event.acceleration || { x: 0, y: 0, z: 0 };
+  if (event.acceleration) {
+    const { x, y, z } = event.acceleration;
+  }
   return (
     <div>
+      {/* <Player> */}
+      {/* //TODO 動画の埋め込みできないなぜ */}
+      {/* <source src="https://youtu.be/gGx5U1esKH0" type="video/mp4" /> */}
+      {/* </Player> */}
       <p>game画面</p>
+      <p>加速度x: {x}</p>
+      <p>加速度x: {y}</p>
+      <p>加速度x: {z}</p>
       <div>
         <Link href="/#">
           <p>back to home</p>
         </Link>
       </div>
-      {/* //TODO 動画埋め込みできてない */}
-      <ReactPlayer
-        url="/video/haikei.mp4"
-        playing={true}
-        loop={false}
-        width={"100%"}
-        height="auto"
-      />
     </div>
   );
 }
 
-export default About;
+export default Game;
--- shake-and-throw/pages/index.tsx
+++ shake-and-throw/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { getWindowSize } from "../hooks/GetWindowSize";
-import  styles from  "../styles/components/Home.module.css";
+import styles from "../styles/components/Home.module.css";
 import Link from "next/link";
 
 //? DeviceMotionEvent: 加速度を取得する
@@ -12,25 +12,23 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   const { height, width } = getWindowSize();
-  // TODO chat-GPTに下の書き方聞いたので、実装してみる
-  /*
-  window.addEventListener("devicemotion", (event) => {
-    console.log(`${event.acceleration.x} m/s2`);
-  });
-  */
 
   return (
     <div className={styles.Home}>
-            <h1 className={styles.title}>タイトル</h1>
+      <h1 className={styles.title}>タイトル</h1>
       <div className={styles.buttons}>
         <Link href="/game">
-        <button className={styles.start} type="button">スタート</button>
+          <button className={styles.start} type="button">
+            スタート
+          </button>
         </Link>
         <Link href="/ranking">
-        <button className={styles.ranking} type='button'>ランキング</button>
+          <button className={styles.ranking} type="button">
+            ランキング
+          </button>
         </Link>
         <Link href="/how_to_playing_game">
-        <button className={styles.rule}>遊び方</button>
+          <button className={styles.rule}>遊び方</button>
         </Link>
       </div>
     </div>
